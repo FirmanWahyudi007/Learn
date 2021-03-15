5d385132dd29b67a0d59e55067359355b56def48
Firman Wahyudi <e41190305@student.polije.ac.id>
2021-03-15 12:04:32 +0700

    delete post

--- restful-api/app/Http/Controllers/PostController.php
+++ restful-api/app/Http/Controllers/PostController.php
@@ -47,4 +47,14 @@ class PostController extends Controller
             ->transformWith(new PostTransformer)
             ->toArray();
     }
+
+    public function delete(Post $post)
+    {
+        $this->authorize('delete', $post);
+        $post->delete();
+
+        return response()->json([
+            'message' => 'Post Deleted',
+        ]);
+    }
 }
